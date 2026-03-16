9d27da5e574330d07ad3df5139b323d0684518f2
Mikael Moilanen <mikael.moilanen@gmail.com>
2026-03-16 08:17:17 +0200

    Add azure auth error handling

--- server/src/auth/azureV2.ts
+++ server/src/auth/azureV2.ts
@@ -1,4 +1,5 @@
 import { decrypt } from '@src/crypto';
+import logger from '@src/logger';
 import { Express } from 'express';
 import { discovery } from 'openid-client';
 import { Strategy, VerifyFunction } from 'openid-client/passport';
@@ -9,12 +10,6 @@ import {
   upsertUser,
 } from '../user';
 
-const oidcDiscoveryOptions = {
-  server: new URL(process.env.AUTH_IDENTITY_METADATA),
-  clientId: process.env.AUTH_CLIENT_ID,
-  clientSecret: process.env.AUTH_CLIENT_SECRET,
-};
-
 /**
  * Configures authentication for given Express application.
  * Initializes express-session and passport middleware to use Azure AD (via
@@ -22,81 +17,92 @@ const oidcDiscoveryOptions = {
  * @param app Express application
  */
 export async function configureAzureAuth(app: Express) {
-  const config = await discovery(
-    oidcDiscoveryOptions.server,
-    oidcDiscoveryOptions.clientId,
-    oidcDiscoveryOptions.clientSecret,
-  );
+  try {
+    const oidcDiscoveryOptions = {
+      server: new URL(process.env.AUTH_IDENTITY_METADATA),
+      clientId: process.env.AUTH_CLIENT_ID,
+      clientSecret: process.env.AUTH_CLIENT_SECRET,
+      redirectUrl: process.env.AUTH_REDIRECT_URL,
+    };
 
-  const verify: VerifyFunction = async (tokenset, done) => {
-    try {
-      const claims = tokenset.claims();
+    const config = await discovery(
+      oidcDiscoveryOptions.server,
+      oidcDiscoveryOptions.clientId,
+      oidcDiscoveryOptions.clientSecret,
+    );
 
-      if (!claims.oid) {
-        return done(new Error('No oid found'));
+    const verify: VerifyFunction = async (tokenset, done) => {
+      try {
+        const claims = tokenset.claims();
+
+        if (!claims.oid) {
+          return done(new Error('No oid found'));
+        }
+
+        const groups = claims.groups as string[] | undefined;
+        if (!groups || groups.length === 0) {
+          return done(new Error('No groups found'));
+        }
+
+        const roles = claims.roles as string[] | undefined;
+        if (!roles || roles.length === 0) {
+          return done(new Error('No roles found'));
+        }
+
+        const user = await upsertUser({
+          id: claims.oid as string,
+          fullName: claims.name as string,
+          email: (claims.email ?? claims.preferred_username) as string,
+          organizations: groups.map((o) => dbOrganizationIdToOrganization(o)),
+          roles,
+        });
+        const userGroups = await getUserGroupsForUser(user.id);
+
+        return done(null, {
+          ...user,
+          groups: [...(user.groups ?? []), ...userGroups.map((g) => g.id)],
+        });
+      } catch (err) {
+        return done(err as Error);
       }
+    };
 
-      const groups = claims.groups as string[] | undefined;
-      if (!groups || groups.length === 0) {
-        return done(new Error('No groups found'));
-      }
+    passport.use(
+      'openid',
+      new Strategy(
+        {
+          config,
+          scope: 'openid profile email',
+          callbackURL: oidcDiscoveryOptions.redirectUrl,
+        },
+        verify,
+      ),
+    );
 
-      const roles = claims.roles as string[] | undefined;
-      if (!roles || roles.length === 0) {
-        return done(new Error('No roles found'));
-      }
-
-      const user = await upsertUser({
-        id: claims.oid as string,
-        fullName: claims.name as string,
-        email: (claims.email ?? claims.preferred_username) as string,
-        organizations: groups.map((o) => dbOrganizationIdToOrganization(o)),
-        roles,
+    // Login route
+    app.get('/login', (req, res, next) => {
+      req.session.redirectUrl = decrypt(req.query.redirect as string);
+      req.session.save(() => {
+        passport.authenticate('openid', {
+          successRedirect: '/',
+          failureRedirect: '/',
+        })(req, res, next);
       });
-      const userGroups = await getUserGroupsForUser(user.id);
-
-      return done(null, {
-        ...user,
-        groups: [...(user.groups ?? []), ...userGroups.map((g) => g.id)],
-      });
-    } catch (err) {
-      return done(err as Error);
-    }
-  };
-
-  passport.use(
-    'openid',
-    new Strategy(
-      {
-        config,
-        scope: 'openid profile email',
-        callbackURL: process.env.AUTH_REDIRECT_URL,
-      },
-      verify,
-    ),
-  );
-
-  // Login route
-  app.get('/login', (req, res, next) => {
-    req.session.redirectUrl = decrypt(req.query.redirect as string);
-    req.session.save(() => {
-      passport.authenticate('openid', {
-        successRedirect: '/',
-        failureRedirect: '/',
-      })(req, res, next);
     });
-  });
 
-  // Callback route for authentication
-  app.get(
-    '/.auth/login/aad/callback',
-    passport.authenticate('openid', {
-      failureRedirect: '/',
-      keepSessionInfo: true, // To keep the redirectUrl
-    }),
-    (req, res) => {
-      const redirectUrl = req.session.redirectUrl ?? '/admin';
-      res.redirect(redirectUrl);
-    },
-  );
+    // Callback route for authentication
+    app.get(
+      '/.auth/login/aad/callback',
+      passport.authenticate('openid', {
+        failureRedirect: '/',
+        keepSessionInfo: true, // To keep the redirectUrl
+      }),
+      (req, res) => {
+        const redirectUrl = req.session.redirectUrl ?? '/admin';
+        res.redirect(redirectUrl);
+      },
+    );
+  } catch (error) {
+    logger.info(`Error configuring Azure auth: ${error.message}`);
+  }
 }
